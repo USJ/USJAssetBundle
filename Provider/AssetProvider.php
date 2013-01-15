3c710d656854112ed77bbbb1d0f1ec0f5b5780e3
Marco Leong <marcogood411@gmail.com>
2013-01-15 23:33:10 +0800

    wip search provider, remove unused test

---
+++ Provider/AssetProvider.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace MDB\AssetBundle\Provider;
+
+use FOQ\ElasticaBundle\Provider\ProviderInterface;
+use Elastica_Type;
+
+class AssetProvider implements ProviderInterface
+{
+    
+    public function populate(\Closure $loggerClosure = null)
+    {
+        if ($loggerClosure) {
+            $loggerClosure('Indexing assets');
+        }
+
+    }
+}
