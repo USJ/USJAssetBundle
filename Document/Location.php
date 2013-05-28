f6e74f3abe8990bd28136eecc3c6333823b536c0
Marco Leong <marcogood411@gmail.com>
2013-05-28 12:55:26 +0800

    add access method

--- Document/Location.php
+++ Document/Location.php
@@ -5,9 +5,94 @@
 
 namespace MDB\AssetBundle\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
+/**
+ * Class Location
+ * @package MDB\AssetBundle\Document
+ *
+ * @MongoDB\MappedSuperclass
+ */
 abstract class Location {
-    protected $geo;
+
+    /**
+     * @MongoDB\EmbedOne
+     */
+    protected $coordinates;
+
+    /**
+     * @MongoDB\String
+     */
     protected $name;
+
+    /**
+     * @MongoDB\String
+     */
     protected $path;
+
+    /**
+     * Set coordinates
+     *
+     * @param $coordinates
+     * @return self
+     */
+    public function setCoordinates($coordinates)
+    {
+        $this->coordinates = $coordinates;
+        return $this;
+    }
+
+    /**
+     * Get coordinates
+     *
+     * @return $coordinates
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return self
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string $path
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
 }
