8bf1cd7475551824f5bac8334e1b1ed377e604f8
Marco Leong <marcogood411@gmail.com>
2013-11-07 17:58:56 +0800

    add basic method to location manager

--- Document/LocationManager.php
+++ Document/LocationManager.php
@@ -12,11 +12,14 @@ class LocationManager {
 
     protected $class;
 
+    protected $repository;
+
     public function __construct($dispatcher, $dm, $class)
     {
         $this->dispatcher = $dispatcher;
         $this->dm = $dm;
         $this->class = $class;
+        $this->repository = $this->dm->getRepository($class);
     }
 
     public function createLocation()
@@ -30,4 +33,28 @@ class LocationManager {
         $this->dm->flush();
     }
 
+    public function findLocationBy(array $criteria)
+    {
+        return $this->repository->findOneBy($criteria);
+    }
+
+    public function findLocationsBy(array $criteria)
+    {
+        return $this->repository->findBy($criteria);
+    }
+
+    public function findLocationById($id)
+    {
+        return $this->repository->findOneById($id);
+    }
+
+    public function findLocationByName($name)
+    {
+        return $this->repository->findOneByName($name);
+    }
+
+    public function getRepository()
+    {
+        return $this->repository;
+    }
 }
